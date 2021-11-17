d9ccf701dbb14ea529bbb55cfc22224d686dbbdd
DouglasHLima <douglashdelima@gmail.com>
2021-11-17 07:22:28 -0300

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputControllerComponent } from './input-controller/input-controller.component';
+import { DisplayLettersComponent } from './display-letters/display-letters.component';
+import { CounterComponent } from './counter/counter.component';
+import { WinnerLabelComponent } from './winner-label/winner-label.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputControllerComponent,
+    DisplayLettersComponent,
+    CounterComponent,
+    WinnerLabelComponent
   ],
   imports: [
     BrowserModule
