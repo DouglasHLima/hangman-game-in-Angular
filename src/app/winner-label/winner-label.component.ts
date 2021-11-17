d0e2381749b791737a0cb8f1020131e6611973b3
DouglasHLima <douglashdelima@gmail.com>
2021-11-17 18:34:13 -0300

    disable reset button for game over

--- src/app/winner-label/winner-label.component.ts
+++ src/app/winner-label/winner-label.component.ts
@@ -7,8 +7,8 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class WinnerLabelComponent implements OnInit {
 
-  @Input() wins:number = 0
-  @Input() result:Boolean = false
+  @Input() wins: number = 0
+  @Input() result: Boolean = false
   
   @Output() resetGame = new EventEmitter()
   @Output() resetAll = new EventEmitter()
